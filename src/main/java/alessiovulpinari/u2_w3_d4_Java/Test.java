51bfc1fee1922664c5475fb78a6acb7dc61e79e7
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-07-04 16:04:03 +0200

    Feature 2

--- src/main/java/alessiovulpinari/u2_w3_d4_Java/Test.java
+++ src/main/java/alessiovulpinari/u2_w3_d4_Java/Test.java
@@ -1,4 +1,14 @@
 package alessiovulpinari.u2_w3_d4_Java;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Test{
+
+    private String name;
+    private String surname;
+    private int age;
 }
+git add
