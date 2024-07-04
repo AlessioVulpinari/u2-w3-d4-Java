f77c86789bdd1a11b317189c2ec3a158c8be0282
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-07-04 16:02:41 +0200

    Feature 1

---
+++ src/main/java/alessiovulpinari/u2_w3_d4_Java/HelloWorld.java
@@ -0,0 +1,11 @@
+package alessiovulpinari.u2_w3_d4_Java;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class HelloWorld implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello world!");
+    }
+}
